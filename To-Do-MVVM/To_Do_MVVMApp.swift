b35b8e5cac66c4f56cf9b00a9390ee854da8b257
Binaya Thapa Magar <binayaekacai96@gmail.com>
2021-09-26 17:48:04 +0545

    First push

---
+++ To-Do-MVVM/To_Do_MVVMApp.swift
@@ -0,0 +1,17 @@
+//
+//  To_Do_MVVMApp.swift
+//  To-Do-MVVM
+//
+//  Created by Binaya on 26/09/2021.
+//
+
+import SwiftUI
+
+@main
+struct To_Do_MVVMApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
